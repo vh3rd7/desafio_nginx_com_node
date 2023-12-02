eb63c3b5e4cf75e2c606bde9d4dae706a4e9e010
Vagner H <vh3rd7@gmail.com>
2023-12-02 10:50:37 -0300

    add express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Full Cycle Rocks!</h1>')
+});
+
+app.listen(port, () => {
+  console.log(`Server running! port: ${port}`)
+});
